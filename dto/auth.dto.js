3e63755253df3c4503b1c115ac87c4e763f6b1ea
Sudheer Reddy <sudheerjanga9999@gmail.com>
2025-07-20 10:46:43 +0530

    solved trim problem

--- dto/auth.dto.js
+++ dto/auth.dto.js
@@ -6,15 +6,42 @@ import logger from "../utility/logger.utility.js";
 
 const GetUserDTO = async (email = null, mobile_number = null) => {
   try {
-    const query = queries.GET_USER;
-    const replacements = {
-      email,
-      mobile_number,
-    };
+    if (email && typeof email === "string") {
+      email = email.trim();
+    }
+
+    if (mobile_number && typeof mobile_number === "string") {
+      mobile_number = mobile_number.trim();
+    }
+
+    if (!email && !mobile_number) {
+      throw new Error("Either email or mobile_number must be provided");
+    }
+
+    let query = `
+      SELECT user_id, first_name, middle_name, last_name, mobile_number, email, dob, password, profile_img, gender, active
+      FROM home_hero.users
+      WHERE 
+    `;
+    const replacements = {};
+
+    if (email && mobile_number) {
+      query += ` mobile_number = :mobile_number OR email = :email`;
+      replacements.email = email;
+      replacements.mobile_number = mobile_number;
+    } else if (email) {
+      query += ` email = :email`;
+      replacements.email = email;
+    } else if (mobile_number) {
+      query += ` mobile_number = :mobile_number`;
+      replacements.mobile_number = mobile_number;
+    }
+
     const data = await mysql.query(query, {
       replacements,
       type: QueryTypes.SELECT,
     });
+
     return data;
   } catch (error) {
     logger.error({ GetUserDTO: error.message });
